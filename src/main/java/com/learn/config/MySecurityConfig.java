3bc6c5f60ccf6dfd2510b3cb655c1a146786d1f7
shailesh <shailesh.d@simformsolutions.com>
2023-04-14 18:18:27 +0530

    Add Admin and User

--- src/main/java/com/learn/config/MySecurityConfig.java
+++ src/main/java/com/learn/config/MySecurityConfig.java
@@ -3,40 +3,52 @@ package com.learn.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class MySecurityConfig{
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http
-                .authorizeHttpRequests((authz)->authz
-                        .anyRequest()
-                        .authenticated()
-                )
-                .httpBasic(withDefaults());
-        return http.build();
-
+        return http.csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/users/").permitAll()
+                .and()
+                .authorizeHttpRequests().requestMatchers("/users/**").authenticated()
+                .and().formLogin().and().build();
     }
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("shailesh")
-                .password("1234")
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails user = User.withUsername("shailesh")
+                .password(passwordEncoder.encode("1234"))
                 .roles("USER")
                 .build();
-        return new InMemoryUserDetailsManager(user);
+        UserDetails admin = User.withUsername("mihir")
+                .password(passwordEncoder.encode("1234"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user,admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 }
--- src/main/java/com/learn/controller/UserController.java
+++ src/main/java/com/learn/controller/UserController.java
@@ -3,6 +3,7 @@ package com.learn.controller;
 import com.learn.models.User;
 import com.learn.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,11 +16,13 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public List<User> getAllUsers(){
         return this.userService.getAllUsers();
     }
 
     @GetMapping("/{username}")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
     public User getUser(@PathVariable("username") String username){
         return this.userService.getUser(username);
     }
